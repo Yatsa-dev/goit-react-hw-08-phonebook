2320efae12d1d025942e717198f0a375aea1557a
Ihor Yatsyshyn <74863323+Yatsa-dev@users.noreply.github.com>
2021-12-02 23:55:28 +0200

    done logIn and logOut

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -1,14 +1,16 @@
+import { useSelector } from 'react-redux';
 import Navigation from '../Navigation/Navigation';
 import AuthNav from '../AuthNav/AuthNav';
 import UserMenu from '../UserMenu/UserMenu';
+import { authSelectors } from 'redux/auth';
 import s from './Appbar.module.css';
 
 export default function Appbar() {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
     <header className={s.header}>
       <Navigation />
-      <AuthNav />
-      <UserMenu />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
 }
--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -4,31 +4,31 @@ import authOperations from './auth-operations';
 const initialState = {
   user: { name: null, email: null },
   token: null,
-  // isLoggedIn: false,
+  isLoggedIn: false,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   extraReducers: {
-    [authOperations.register](state, action) {
+    [authOperations.register.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
-      // state.isLoggedIn = true;
+      state.isLoggedIn = true;
     },
-    [authOperations.logIn](state, action) {
+    [authOperations.logIn.fulfilled](state, action) {
       state.user = action.payload.user;
       state.token = action.payload.token;
-      // state.isLoggedIn = true;
+      state.isLoggedIn = true;
     },
-    [authOperations.logOut](state, action) {
+    [authOperations.logOut.fulfilled](state, action) {
       state.user = { name: null, email: null };
       state.token = null;
-      // state.isLoggedIn = false;
+      state.isLoggedIn = false;
     },
-    [authOperations.fetchCurrentUser](state, action) {
+    [authOperations.fetchCurrentUser.fulfilled](state, action) {
       state.user = action.payload;
-      // state.isLoggedIn = true;
+      state.isLoggedIn = true;
     },
   },
 });
